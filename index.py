932fe09f12e0150f751540d5cd1ac5fd7691699b
Ander Garmendia <kelertxiki@gmail.com>
2019-01-13 23:43:14 +0100

    Update sample process to get and analyze data

--- index.py
+++ index.py
@@ -1,16 +1,26 @@
 import requests
 import lxml.html
+import sys
 
-from parsers.act import ActAgeParser
+from parsers.actageparser import ActAgeParser
 from parsers.arc1ageparser import Arc1AgeParser
 from parsers.arc2ageparser import Arc2AgeParser
-
+from parsers.euskotrenageparser import EuskotrenAgeParser
+from parsers.eteageparser import EteAgeParser
 
 if __name__ == '__main__':
-
-  arc2AgeParser = Arc2AgeParser()
-  arc2AgeParser.analize()
-  # for staff in arc2AgeParser.staff:
-    # arc2AgeParser.get_club_data(staff['name'], staff['url'])
-    # arc2AgeParser.get_rowers_data(staff['name'])
-
+    year = sys.argv[1]
+    liga = sys.argv[2]
+    if liga == 'act':
+        parser = ActAgeParser()
+    elif liga == 'arc1':
+        parser = Arc1AgeParser()
+    elif liga == 'arc2':
+        parser = Arc2AgeParser()
+    parser.staff = parser.get_clubs_in_year(year, liga)
+    for club in parser.staff:
+        if club['name'] != 'C.N. Luanco':
+            continue
+        parser.get_plantilla_page(club['name'], year, club['url'])
+        parser.get_rowers_data(club['name'], year)
+    parser.analize(year)
