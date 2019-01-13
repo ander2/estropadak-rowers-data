f5fea9ebcbf013d855c261580a1233d59c7d8f45
Ander Garmendia <kelertxiki@gmail.com>
2019-01-13 23:41:17 +0100

    Add year and unify with ACT parser

--- parsers/arc1ageparser.py
+++ parsers/arc1ageparser.py
@@ -1,109 +1,149 @@
 import requests
 import lxml.html
 import glob
+import os
+import re
+
 
 class Arc1AgeParser:
-  url_base = 'http://www.liga-arc.com/es/'
-  file_path = './pages/arc1'
-  clubs = [
-    {"url": "/clubes/index.php?id=es&c=2", "name": "cabo"},
-    {"url": "/clubes/index.php?id=es&c=4", "name": "tiran"},
-    {"url": "/clubes/index.php?id=es&c=8", "name": "hondarribia"},
-    {"url": "/clubes/index.php?id=es&c=9", "name": "orio"},
-    {"url": "/clubes/index.php?id=es&c=38", "name": "kaiku"},
-    {"url": "/clubes/index.php?id=es&c=46", "name": "ondarroa"},
-    {"url": "/clubes/index.php?id=es&c=94", "name": "donostiarra"},
-    {"url": "/clubes/index.php?id=es&c=11", "name": "sanjuan"},
-    {"url": "/clubes/index.php?id=es&c=18", "name": "sanpedro"},
-    {"url": "/clubes/index.php?id=es&c=37", "name": "santurtzi"},
-    {"url": "/clubes/index.php?id=es&c=13", "name": "urdaibai"},
-    {"url": "/clubes/index.php?id=es&c=68", "name": "zierbena"},
-  ]
+    url_base = 'http://www.liga-arc.com/es/'
+    file_path = './pages/arc1'
+    staff = [ ]
 
-  staff = [
-    {'url': 'clubes/2018/95/1/arkote-helvetia/plantilla', 'name': 'ARKOTE-HELVETIA'},
-    {'url': 'clubes/2018/32/1/astillero-san-jose-xv/plantilla', 'name': 'ASTILLERO-SAN JOSE XV'},
-    {'url': 'clubes/2018/99/1/deusto-bilbao/plantilla', 'name': 'DEUSTO-BILBAO'},
-    {'url': 'clubes/2018/19/1/getaria/plantilla', 'name': 'GETARIA'},
-    {'url': 'clubes/2018/6/1/hibaika/plantilla', 'name': 'HIBAIKA'},
-    {'url': 'clubes/2018/89/1/lekittarra-elecnor-bm/plantilla', 'name': 'LEKITTARRA-ELECNOR-BM'},
-    {'url': 'clubes/2018/235/1/lapurdi/plantilla', 'name': 'LAPURDI'},
-    {'url': 'clubes/2018/40/1/sdr-pedrea/plantilla', 'name': 'SDR PEDRENA'},
-    {'url': 'clubes/2018/102/1/portugalete/plantilla', 'name': 'PORTUGALETE'},
-    {'url': 'clubes/2018/3/1/san-juan-sumelec/plantilla', 'name': 'SAN JUAN-SUMELEC'},
-    {'url': 'clubes/2018/16/1/zarautz-babyauto/plantilla', 'name': 'ZARAUTZ-BABYAUTO'},
-    {'url': 'clubes/2018/17/1/zumaia-salegi-jatetxea/plantilla', 'name': 'ZUMAIA-SALEGI JATETXEA'}
-  ]
+    def get_main_page(self):
+        main_page = requests.get(f'{self.url_base}clubes')
+        with open('./liga-arc.html', 'w', encoding='utf-8') as f:
+            f.write(main_page.text)
+        f.close()
 
-  def get_main_page(self):
-    main_page = requests.get(f'{self.url_base}clubes')
-    with open('./liga-arc.html', 'w', encoding='utf-8') as f:
-      f.write(main_page.text)
-    f.close()
+    def get_clubs_in_year(self, year, liga):
+        clubs = []
+        liga_id = 1
+        if liga == 'arc2':
+            liga_id = 2
+        stats = requests.get(f'http://estropadak.net/api/sailkapena?league={liga}&year={year}').json()
+        izenak = sorted(list(stats[0]['stats'].keys()))
+        liga_clubs = {}
+        with open(f'./taldeak_{liga}.txt', 'r', encoding='utf-8') as f:
+            for line in f:
+                l = line.strip().split(' ', maxsplit=1)
+                liga_clubs[l[1]] = l[0]
+                print(l[1])
+        f.close()
+        for izena in izenak:
+            try:
+                id = liga_clubs[izena]
+                _izena = izena.replace('ñ','')
+                clubs.append({"name": izena, "url":f'/clubes/{year}/{id}/{liga_id}/{_izena}/plantilla' })
+            except Exception:
+                print(f'No match for: {izena}')
+        return clubs
 
-  def get_club_data(self, club, url):
-    print(f'Getting data for {club}: {self.url_base}{url}')
-    page = requests.get(f'{self.url_base}{url}')
-    with open(f'{self.file_path}/{club}.html', 'w', encoding='utf-8') as f:
-      f.write(page.text)
-    f.close()
+    def get_plantilla_page(self, club, year, url):
+        print(f'Getting page for {club}: {self.url_base}{url}')
+        page = requests.get(f'{self.url_base}{url}')
+        try:
+            os.stat(f'{self.file_path}/{year}')
+        except FileNotFoundError:
+            os.mkdir(f'{self.file_path}/{year}')
+        with open(f'{self.file_path}/{year}/{club}.html', 'w', encoding='utf-8') as f:
+            f.write(page.text)
+        f.close()
 
-  def get_rowers_data(self, club):
-    with open(f'{self.file_path}/{club}.html', 'r', encoding='utf-8') as f:
-      document = lxml.html.fromstring(f.read())
-      links = document.cssselect('.ver-ficha a')
-      for link in links:
-        link_lst = link.get('href').split('/')
-        rower_name = link_lst[-1:]
-        print(f'Getting data for {club} {rower_name}')
-        page = requests.get(link.get('href'))
-        with open(f'{self.file_path}/{club}-{rower_name}.html', 'w', encoding='utf-8') as f2:
-          f2.write(page.text)
-        continue
-    f.close()
+    def get_rowers_data(self, club, year):
+        with open(f'{self.file_path}/{year}/{club}.html', 'r', encoding='utf-8') as f:
+            document = lxml.html.fromstring(f.read())
+            links = document.cssselect('.ver-ficha a')
+            for link in links:
+                link_lst = link.get('href').split('/')
+                rower_name = link_lst[-1:]
+                print(f'Getting data for {club} {rower_name}')
+                page = requests.get(link.get('href'))
+                with open(f'{self.file_path}/{year}/{club}-{rower_name}.html', 'w', encoding='utf-8') as f2:
+                    f2.write(page.text)
+                    continue
+        f.close()
+
+    def parse_years_in_rowing(self, content):
+        counter = 0
+        bad_name = content.cssselect('.nombre-apellidos')[0].text_content()
+        full_name = re.sub('([A-Z])', r' \1', bad_name)
+        name = full_name.strip()
+        for year in content.cssselect('div.historial table tbody tr'):
+            cols = year.cssselect('td.club span')
+            if len(cols) > 0:
+                counter += 1
+        return (name, counter)
 
 
-  def parse_rower_data(self, content):
-    name = ''
-    name = content.cssselect('.nombre')[0].text.strip()
-    age = int(content.cssselect('.edad strong')[0].text.strip())
-    print(f'{name} {age}')
-    return (name, age)
+    def parse_rower_data(self, content):
+        name = ''
+        name = content.cssselect('.nombre')[0].text.strip()
+        age = int(content.cssselect('.edad strong')[0].text.strip())
+        print(f'{name} {age}')
+        return (name, age)
 
-  def analize_staff_data(self, data):
-    ages = []
-    for d in data:
-      age = d[1]
-      ages.append(age)
-    average = sum(ages)/len(ages)
-    return average
+    def analize_staff_data(self, data):
+        ages = []
+        average = 0
+        for d in data:
+            age = d[1]
+            ages.append(age)
+            average = sum(ages)/len(ages)
+        return average
 
-  def isRower(self, content):
-    title = content.cssselect('.fizda')
-    if title[0].text.strip() == 'Remero':
-      return True
-    else:
-      return False
 
-  def parse_staff_data(self, club):
-    staff_data = []
-    print(club)
-    for pathname in glob.glob(f'./{self.file_path}/{club}-*'):
-      try:
-        with open(pathname, 'r', encoding='utf-8') as f:
-          document = lxml.html.fromstring(f.read())
+    def analize_rowing_years(self, data):
+        years = []
+        if len(data) > 0:
+            for d in data:
+                print(f'{d[0]} {d[1]}')
+                years.append(d[1])
+            average = sum(years)/len(years)
+            return average
+        else:
+            return None
 
-          data = self.parse_rower_data(document)
-        staff_data.append(data)
-      except IndexError:
+    def isRower(self, content):
+        title = content.cssselect('.fizda')
+        if title[0].text.strip() == 'Remero':
+            return True
+        else:
+            return False
+
+    def parse_staff_deep_data(self, club):
+        staff_data = []
+        for pathname in glob.glob(f'{self.file_path}/{club}-*'):
+            with open(pathname, 'r', encoding='utf-8') as f:
+                document = lxml.html.fromstring(f.read())
+                data = self.parse_years_in_rowing(document)
+            staff_data.append(data)
+        return self.analize_rowing_years(staff_data)
+
+    def parse_staff_data(self, club, year):
+        staff_data = []
+        for pathname in glob.glob(f'{self.file_path}/{year}/{club}-*'):
+            try:
+                print(pathname)
+                with open(pathname, 'r', encoding='utf-8') as f:
+                    document = lxml.html.fromstring(f.read())
+                    data = self.parse_rower_data(document)
+                    staff_data.append(data)
+            except IndexError:
+                pass
+        return self.analize_staff_data(staff_data)
+
+    def analize_years(self):
+        for club in self.staff:
+            average = self.parse_staff_deep_data(club['name'])
+            print(f'{club["name"]}: {average}')
+            print(10*'-')
+
+    def analize(self, year):
+        for club in self.staff:
+            average = self.parse_staff_data(club['name'], year)
+            print(f'{club["name"]}: {average}')
+            print(10*'-')
+
+    def __init__(self):
         pass
-    return self.analize_staff_data(staff_data)
-
-  def analize(self):
-    for club in self.staff:
-        average = self.parse_staff_data(club['name'])
-        print(f'{club["name"]}: {average}')
-        print(10*'-')
-
-  def __init__(self):
-    pass
--- parsers/arc2ageparser.py
+++ parsers/arc2ageparser.py
@@ -1,39 +1,36 @@
+import requests
 from parsers.arc1ageparser import Arc1AgeParser
 
+
 class Arc2AgeParser(Arc1AgeParser):
-  url_base = 'http://www.liga-arc.com/es/'
-  file_path = './pages/arc2'
-  clubs = [
-      {"url": "/clubes/index.php?id=es&c=2", "name": "cabo"},
-      {"url": "/clubes/index.php?id=es&c=4", "name": "tiran"},
-      {"url": "/clubes/index.php?id=es&c=8", "name": "hondarribia"},
-      {"url": "/clubes/index.php?id=es&c=9", "name": "orio"},
-      {"url": "/clubes/index.php?id=es&c=38", "name": "kaiku"},
-      {"url": "/clubes/index.php?id=es&c=46", "name": "ondarroa"},
-      {"url": "/clubes/index.php?id=es&c=94", "name": "donostiarra"},
-      {"url": "/clubes/index.php?id=es&c=11", "name": "sanjuan"},
-      {"url": "/clubes/index.php?id=es&c=18", "name": "sanpedro"},
-      {"url": "/clubes/index.php?id=es&c=37", "name": "santurtzi"},
-      {"url": "/clubes/index.php?id=es&c=13", "name": "urdaibai"},
-      {"url": "/clubes/index.php?id=es&c=68", "name": "zierbena"},
-  ]
+    url_base = 'http://www.liga-arc.com/es/'
+    file_path = './pages/arc2'
 
-  staff = [
-      {'url': 'clubes/2018/92/1/bermeo-sumoil/plantilla', 'name': 'BERMEO-SUMOIL'},
-      {'url': 'clubes/2018/91/1/elantxobe-idar/plantilla', 'name': 'ELANTXOBE-IDAR'},
-      {'url': 'clubes/2018/31/1/camargo/plantilla', 'name': 'CAMARGO'},
-      {'url': 'clubes/2018/28/1/castrea/plantilla', 'name': 'Castreña'},
-      {'url': 'clubes/2018/29/1/colindres/plantilla', 'name': 'Colindres'},
-      {'url': 'clubes/2018/39/1/laredo-la-pejinuca/plantilla', 'name': 'LAREDO-LA PEJINUCA'},
-      {'url': 'clubes/2018/12/1/donostiarra/plantilla', 'name': 'DONOSTIARRA'},
-      {'url': 'clubes/2018/96/1/getxo/plantilla', 'name': 'Getxo'},
-      {'url': 'clubes/2018/1/1/hondarribia/plantilla', 'name': 'HONDARRIBIA'},
-      {'url': 'clubes/2018/98/1/lutxana-ae/plantilla', 'name': 'LUTXANA AE'},
-      {'url': 'clubes/2018/18/1/mutriku/plantilla', 'name': 'MUTRIKU'},
-      {'url': 'clubes/2018/14/1/orio-babyauto/plantilla', 'name': 'ORIO-BABYAUTO'},
-      {'url': 'clubes/2018/4/1/san-pedro/plantilla', 'name': 'SAN PEDRO'},
-      {'url': 'clubes/2018/37/1/irc-santoa/plantilla', 'name': 'IRC SANTOÑA'},
-  ]
+    def __init__(self):
+        super()
 
-def __init__(self):
-  super()
+    def get_clubs_in_year(self, year, liga):
+        clubs = []
+        talde_id = 2
+        stats = requests.get(f'http://estropadak.net/api/sailkapena?league={liga}&year={year}').json()
+        izenak = sorted(list(stats[0]['stats'].keys()))
+        liga_clubs = {}
+        with open(f'./taldeak_{liga}.txt', 'r', encoding='utf-8') as f:
+            for line in f:
+                l = line.strip().split(' ', maxsplit=1)
+                liga_clubs[l[1]] = l[0]
+                print(l[1])
+        f.close()
+        for izena in izenak:
+            try:
+                id = liga_clubs[izena]
+                _izena = izena.replace('ñ','')
+                url = f'/clubes/{year}/{id}/{talde_id}/{_izena}/plantilla'
+                r = requests.head(self.url_base + url)
+                if r.status_code == 404:
+                    talde_id = 1
+                    url = f'/clubes/{year}/{id}/{talde_id}/{_izena}/plantilla'
+                clubs.append({"name": izena, "url":f'/clubes/{year}/{id}/{talde_id}/{_izena}/plantilla' })
+            except Exception:
+                print(f'No match for: {izena}')
+        return clubs
