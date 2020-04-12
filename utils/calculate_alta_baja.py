d22055349fddc1502ac357e607ff79ec6b0acc4b
Ander Garmendia <kelertxiki@gmail.com>
2020-04-12 09:03:25 +0200

    Improve rower name comparison. Add logging.

--- utils/calculate_alta_baja.py
+++ utils/calculate_alta_baja.py
@@ -1,3 +1,15 @@
+import logging 
+
+logger = logging.getLogger('estropadak')
+logger.setLevel('DEBUG')
+
+def compare_rower_name(r1, r2):
+    r1 = r1.lower()
+    r2 = r2.lower()
+    trans = str.maketrans('áéíóú','aeiou')
+    return r1.translate(trans) == r2.translate(trans)
+
+
 def calculate_alta_baja(aurtengoa, aurrekoa, team):
     ''' 
         Taken staff from this year and previous year, return
@@ -8,17 +20,17 @@ def calculate_alta_baja(aurtengoa, aurrekoa, team):
 
     aurtengo_arraunlariak = aurtengoa[team]
     aurreko_arraunlariak = aurrekoa[team]
-    print(team)
+    logger.debug('Calculating alta-baja for %s', team)
     for a in aurtengo_arraunlariak:
-        print(a.name)
         izena = a.name
-        badago = [b.name for b in aurreko_arraunlariak if a.name == b.name]
+        badago = [b.name for b in aurreko_arraunlariak if compare_rower_name(a.name, b.name)]
         if len(badago) == 0:
             altak.append(izena)
 
     for a in aurreko_arraunlariak:
-        badago = [b.name for b in aurtengo_arraunlariak if a.name == b.name]
+        badago = [b.name for b in aurtengo_arraunlariak if compare_rower_name(a.name, b.name)]
         if len(badago) == 0:
             bajak.append(a.name)
 
+    logger.debug('Calculating alta-baja for %s finished', team)
     return altak, bajak
