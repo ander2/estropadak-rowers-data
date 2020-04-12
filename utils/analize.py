319b4e734bcabca1bd44b43f5b89c2df675a2c50
Ander Garmendia <kelertxiki@gmail.com>
2020-04-12 09:02:52 +0200

    Refactor script: - Add argument handling - Support analisys per year - PEP8 compliance

--- utils/analize.py
+++ utils/analize.py
@@ -1,8 +1,7 @@
 import csv
-import sys
+import click
 import datetime
 import pickle
-from operator import attrgetter
 
 from parsers.actageparser import ActAgeParser
 from parsers.arc1ageparser import Arc1AgeParser
@@ -11,23 +10,51 @@ from parsers.euskotrenageparser import EuskotrenAgeParser
 from parsers.eteageparser import EteAgeParser
 
 
+def load_data(liga):
+    ''' Load serialized data '''
+    data = None
+    with open(f'./results/{liga.lower()}_data', 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+
+def save_data(liga: str, data):
+    with open(f'./results/{liga}_data', 'wb') as f:
+        pickle.dump(data, f)
+
+
 def write_to_csv(year, data):
     with open(f'./results/{year}.csv', 'w', newline='') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter='\t',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for taldea in data:
-            adin_arabera = sorted(data[taldea], key=lambda arraunlari: datetime.datetime.strptime(arraunlari.birthday, '%d-%m-%Y')) # attrgetter('birthday'))# 
+            adin_arabera = sorted(
+                data[taldea],
+                key=lambda arraunlari: datetime.datetime.strptime(
+                    arraunlari.birthday,
+                    '%d-%m-%Y'
+                )
+            )
             spamwriter.writerow([taldea])
             for arraunlari in adin_arabera:
-                spamwriter.writerow([arraunlari.name, arraunlari.birthday, arraunlari.birthplace])
+                spamwriter.writerow([
+                    arraunlari.name,
+                    arraunlari.birthday,
+                    arraunlari.birthplace
+                ])
 
 
-
-if __name__ == '__main__':
-    liga = sys.argv[1]
-    result = {}
+@click.command()
+@click.option('--year', type=click.INT, help='Year to analize')
+@click.argument(
+    'liga',
+    type=click.Choice([
+        'act', 'arc1', 'arc2', 'euskotren', 'ete'
+    ]))
+def analize(year, liga):
     start_year = 2009
-    end_year = 2020
+    end_year = 2021
+    team_data = load_data(liga)
     if liga == 'act':
         parser = ActAgeParser()
     elif liga == 'arc1':
@@ -42,9 +69,21 @@ if __name__ == '__main__':
     elif liga == 'ete':
         parser = EteAgeParser()
         start_year = 2018
-    for year in range(start_year, end_year):
-        parser.staff = parser.get_clubs_in_year(year, liga)
-        data = parser.analize(year)
-        result[year] = data
-    with open(f'./results/{liga}_data', 'wb') as f:
-        pickle.dump(result, f)
+    if not year:
+        for year in range(start_year, end_year):
+            parser.staff = parser.get_clubs_in_year(year, liga)
+            data = parser.analize(year)
+            team_data[year] = data
+    else:
+        start_year = year
+        end_year = year + 1
+        for year in range(start_year, end_year):
+            parser.staff = parser.get_clubs_in_year(year, liga)
+            data = parser.analize(year)
+            team_data[year] = data
+    save_data(liga, team_data)
+
+
+if __name__ == '__main__':
+    analize()
+ 
