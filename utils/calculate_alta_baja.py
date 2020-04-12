497bcea0119b08bd89e46447a285dedf9aa9c2a3
Ander Garmendia <kelertxiki@gmail.com>
2020-04-12 09:08:47 +0200

    PEP8 fixes

--- utils/calculate_alta_baja.py
+++ utils/calculate_alta_baja.py
@@ -1,20 +1,21 @@
-import logging 
+import logging
 
 logger = logging.getLogger('estropadak')
 logger.setLevel('DEBUG')
 
+
 def compare_rower_name(r1, r2):
     r1 = r1.lower()
     r2 = r2.lower()
-    trans = str.maketrans('áéíóú','aeiou')
+    trans = str.maketrans('áéíóú', 'aeiou')
     return r1.translate(trans) == r2.translate(trans)
 
 
 def calculate_alta_baja(aurtengoa, aurrekoa, team):
-    ''' 
+    '''
         Taken staff from this year and previous year, return
         how many new rowers and rowers left there are
-    ''' 
+    '''
     altak = []
     bajak = []
 
@@ -23,12 +24,20 @@ def calculate_alta_baja(aurtengoa, aurrekoa, team):
     logger.debug('Calculating alta-baja for %s', team)
     for a in aurtengo_arraunlariak:
         izena = a.name
-        badago = [b.name for b in aurreko_arraunlariak if compare_rower_name(a.name, b.name)]
+        badago = [
+            b.name
+            for b in aurreko_arraunlariak
+            if compare_rower_name(a.name, b.name)
+        ]
         if len(badago) == 0:
             altak.append(izena)
 
     for a in aurreko_arraunlariak:
-        badago = [b.name for b in aurtengo_arraunlariak if compare_rower_name(a.name, b.name)]
+        badago = [
+            b.name
+            for b in aurtengo_arraunlariak
+            if compare_rower_name(a.name, b.name)
+        ]
         if len(badago) == 0:
             bajak.append(a.name)
 
